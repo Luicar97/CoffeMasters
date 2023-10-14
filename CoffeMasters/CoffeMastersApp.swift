0d52d90cd6b09de0e10178cb235a488db4c7869b
Luicar97 <carlos_mendoza160@hotmail.com>
2023-10-14 12:50:16 -0500

    Initial Commit

---
+++ CoffeMasters/CoffeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMastersApp.swift
+//  CoffeMasters
+//
+//  Created by Carlos Mendoza Garay on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
